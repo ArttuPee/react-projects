a73b9f70b8c71a6e6d619ca0689a1ffdf4d0eeab
Arttu <arttu.pyykonen105@gmail.com>
2021-03-30 17:08:19 +0300

    Synttäri muistuttaja omalla datalla

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ people }) => {
+  const getAge = (dateString) => {
+    let today = new Date();
+    let birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  const getNextBirthDay = (person) => {
+    let next = new Date(person.birthday);
+    let year = next.getFullYear();
+    let today = new Date();
+    while (next < today) {
+      year++;
+      next.setFullYear(year);
+    }
+    return next;
+  };
+
+  const getDaysTo = (date) => {
+    let today = new Date();
+
+    return Math.ceil((date.getTime() - today.getTime()) / (1000 * 3600 * 24));
+  };
+
   return (
     <>
-      <h2>list component</h2>
+      {people
+        .sort((a, b) => getNextBirthDay(a) - getNextBirthDay(b))
+        .map((person) => {
+          let { id, name, age, image } = person;
+          if (person.birthday !== undefined) {
+            age = getAge(person.birthday);
+          }
+          let daysToNextBD = getDaysTo(getNextBirthDay(person));
+          let birthdayString =
+            getNextBirthDay(person).getDate() +
+            "." +
+            (getNextBirthDay(person).getMonth()+1) +
+            "." +
+            getNextBirthDay(person).getUTCFullYear();
+
+          return (
+            <article key={id} className="person">
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>
+                  {age + 1} years in {daysToNextBD} days
+                </p>
+                <p>{birthdayString}</p>
+              </div>
+            </article>
+          );
+        })}
     </>
   );
 };
--- 1-birthday-reminder/setup/src/data.js
+++ 1-birthday-reminder/setup/src/data.js
@@ -1,37 +1,35 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: "Arttu Pyykönen",
+    age: 25,
+    birthday: "1999-3-22",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      "https://rahavelho.fi/wp-content/uploads/2021/01/IMG_0231-2-min-scaled-e1610550997716.jpg",
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: "Akseli Puumalainen",
+    age: 22,
+    birthday: "1999-2-1",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      "https://rahavelho.fi/wp-content/uploads/2021/01/Akseli-Puumalainen-scaled-e1610547368281.jpg",
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: "Elias Räinä",
+    age: 21,
+    birthday: "1999-9-20",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      "https://rahavelho.fi/wp-content/uploads/2021/01/Elias-Raina-min-scaled-e1610557332363.jpg",
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: "Pinja Repo",
+    age: 21,
+    birthday: "1999-4-15",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      "https://rahavelho.fi/wp-content/uploads/2021/01/Pinja-Repo-scaled-e1610547462382.jpg",
   },
-  {
-    id: 5,
-    name: 'Lola Gardner',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
+
 ];
