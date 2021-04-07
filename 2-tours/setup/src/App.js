6ad769a30213a1c4ede64ac6cca37ebab278fd0c
Arttu <arttu.pyykonen105@gmail.com>
2021-04-07 10:55:53 +0300

    Lisää tours tehtävä

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+/*
+Tästä projektista voi tehdä omanlaisen käyttämälle esimerkiksi trivago apia ja etsimällä sieltä kaikki tampereella vapaat hotellit yms. 
+
+trivago api https://developer.trivago.com/expressbooking/api-workflow.html
+
+Ilmeisesti apit ei yleensä ole ilmaisia tai niissä on jokin catch, mutta alta löytyy lista muutamasta ilmeisesti helposta
+
+https://rapidapi.com/collection/best-apis-for-beginners
+
+
+Todennäköisesti teen oman pikku APIn nettisivuille ja laitan sinne omat kuvat ja tekstit. Näin se vaikuttaa siltä että osaan tehdä jotain ja saa vähän personallisuutta. 
+
+*/
+
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours)
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={()=> fetchTours()}>refresh</button>
+      </div>
+    </main>
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  );
 }
 
-export default App
+export default App;
